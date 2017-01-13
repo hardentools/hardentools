64512d74ba15936c699375334357a2a78672d465
Nex <nex@nex.sx>
2017-01-14 00:27:47 +0100

    Added preliminary GUI

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# go get github.com/lxc/win
+# go get github.com/lxc/walk
+# go get github.com/akavel/rsrc
+# go get golang.org/x/sys/windows/registry
+
+$GOPATH/bin/rsrc -manifest harden.manifest -o rsrc.syso
 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 go build --ldflags '-s -w -extldflags "-static"' -o Hardentools.exe
--- main.go
+++ main.go
@@ -19,13 +19,33 @@
 package main
 
 import (
-    "fmt"
-    "flag"
     "golang.org/x/sys/windows/registry"
+    "github.com/lxn/walk"
+    . "github.com/lxn/walk/declarative"
 )
 
+const harden_key_path = "SOFTWARE\\Security Without Borders\\"
+
+func check_status() bool {
+    key, err := registry.OpenKey(registry.CURRENT_USER, harden_key_path, registry.READ)
+    if err != nil {
+        return false
+    }
+
+    value, _, err := key.GetIntegerValue("Harden")
+    if err != nil {
+        return false
+    }
+
+    if value == 1 {
+        return true
+    } else {
+        return false
+    }
+}
+
 func mark_status(is_active bool) {
-    key, _, err := registry.CreateKey(registry.CURRENT_USER, "SOFTWARE\\Security Without Borders\\", registry.WRITE)
+    key, _, err := registry.CreateKey(registry.CURRENT_USER, harden_key_path, registry.WRITE)
     if err != nil {
         panic(err)
     }
@@ -37,31 +57,59 @@ func mark_status(is_active bool) {
     }
 }
 
+func disable_all() {
+    trigger_wsh(false)
+    trigger_ole(false)
+    trigger_macro(false)
+    trigger_pdf_js(false)
+    trigger_pdf_objects(false)
+
+    mark_status(true)
+}
+
+func enable_all() {
+    trigger_wsh(true)
+    trigger_ole(true)
+    trigger_macro(true)
+    trigger_pdf_js(true)
+    trigger_pdf_objects(true)
+
+    mark_status(false)    
+}
+
 func main() {
-    restore := flag.Bool("restore", false, "Restore original settings")
-    flag.Parse()
+    var window *walk.MainWindow
+    var events *walk.TextEdit
+    var button_text, events_text string
+    var button_func func()
 
-    if *restore {
-        fmt.Println("Restoring original settings...")
-
-        trigger_wsh(true)
-        trigger_ole(true)
-        trigger_macro(true)
-        trigger_pdf_js(true)
-        trigger_pdf_objects(true)
-
-        mark_status(false)
+    if check_status() == false {
+        button_text = "Harden!"
+        button_func = disable_all
+        events_text = "Ready to harden some features of your system?"
     } else {
-        fmt.Println("Disabling dangerous features...")
-
-        trigger_wsh(false)
-        trigger_ole(false)
-        trigger_macro(false)
-        trigger_pdf_js(false)
-        trigger_pdf_objects(false)
-
-        mark_status(true)
+        button_text = "Restore..."
+        button_func = enable_all
+        events_text = "We have already hardened some risky features, do you want to restore them?"
     }
 
-    fmt.Println("Done!")
+    MainWindow{
+        AssignTo: &window,
+        Title: "Harden - Security Without Borders",
+        MinSize: Size{400, 300},
+        Layout:  VBox{},
+        Children: []Widget{
+            TextEdit{
+                AssignTo: &events,
+                Text: events_text,
+                ReadOnly: true,
+            },
+            PushButton{
+                Text: button_text,
+                OnClicked: button_func,
+            },
+        },
+    }.Create()
+
+    window.Run()
 }
