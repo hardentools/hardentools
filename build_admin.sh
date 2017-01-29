a276a15947c1e15f0ea86e0e5c96ccf77b20f541
obsti8383 <github@florian-probst.de>
2017-01-29 15:23:28 +0100

    changed filename for admin exe

--- build_admin.sh
+++ build_admin.sh
@@ -6,4 +6,4 @@
 # go get golang.org/x/sys/windows/registry
 
 $GOPATH/bin/rsrc -manifest harden_admin.manifest -o rsrc.syso
-GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 go build --ldflags '-s -w -extldflags "-static"' -o Hardentools.exe
+GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 go build --ldflags '-s -w -extldflags "-static"' -o Hardentools_admin.exe
