36ef8c8a370b89a2d3e0262ce0de048ff97c9b90
obsti8383 <github@florian-probst.de>
2017-01-29 15:48:50 +0100

    fix indentation to spaces, add readme for powershell

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 /*
-   Hardentools
-   Copyright (C) 2017  Claudio Guarnieri, Mariano Graziano
+    Hardentools
+    Copyright (C) 2017  Claudio Guarnieri, Mariano Graziano
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 package main
 
 import (
-	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
-	"golang.org/x/sys/windows/registry"
+    "github.com/lxn/walk"
+    . "github.com/lxn/walk/declarative"
+    "golang.org/x/sys/windows/registry"
 )
 
 var window *walk.MainWindow
@@ -30,93 +29,93 @@ var events *walk.TextEdit
 const harden_key_path = "SOFTWARE\\Security Without Borders\\"
 
 func check_status() bool {
-	key, err := registry.OpenKey(registry.CURRENT_USER, harden_key_path, registry.READ)
-	if err != nil {
-		return false
-	}
+    key, err := registry.OpenKey(registry.CURRENT_USER, harden_key_path, registry.READ)
+    if err != nil {
+        return false
+    }
 
-	value, _, err := key.GetIntegerValue("Harden")
-	if err != nil {
-		return false
-	}
+    value, _, err := key.GetIntegerValue("Harden")
+    if err != nil {
+        return false
+    }
 
-	if value == 1 {
-		return true
-	} else {
-		return false
-	}
+    if value == 1 {
+        return true
+    } else {
+        return false
+    }
 }
 
 func mark_status(is_active bool) {
-	key, _, err := registry.CreateKey(registry.CURRENT_USER, harden_key_path, registry.WRITE)
-	if err != nil {
-		panic(err)
-	}
+    key, _, err := registry.CreateKey(registry.CURRENT_USER, harden_key_path, registry.WRITE)
+    if err != nil {
+        panic(err)
+    }
 
-	if is_active {
-		key.SetDWordValue("Harden", 1)
-	} else {
-		key.SetDWordValue("Harden", 0)
-	}
+    if is_active {
+        key.SetDWordValue("Harden", 1)
+    } else {
+        key.SetDWordValue("Harden", 0)
+    }
 }
 
 func disable_all() {
-	trigger_wsh(false)
-	trigger_ole(false)
-	trigger_macro(false)
-	trigger_activex(false)
-	trigger_pdf_js(false)
-	trigger_pdf_objects(false)
-	trigger_autorun(false)
-	trigger_powershell(false)
+    trigger_wsh(false)
+    trigger_ole(false)
+    trigger_macro(false)
+    trigger_activex(false)
+    trigger_pdf_js(false)
+    trigger_pdf_objects(false)
+    trigger_autorun(false)
+    trigger_powershell(false)
 
-	mark_status(true)
+    mark_status(true)
 }
 
 func enable_all() {
-	trigger_wsh(true)
-	trigger_ole(true)
-	trigger_macro(true)
-	trigger_activex(true)
-	trigger_pdf_js(true)
-	trigger_pdf_objects(true)
-	trigger_autorun(true)
-	trigger_powershell(true)
+    trigger_wsh(true)
+    trigger_ole(true)
+    trigger_macro(true)
+    trigger_activex(true)
+    trigger_pdf_js(true)
+    trigger_pdf_objects(true)
+    trigger_autorun(true)
+    trigger_powershell(true)
 
-	mark_status(false)
+    mark_status(false)
 }
 
 func main() {
-	var button_text, events_text string
-	var button_func func()
+    var button_text, events_text string
+    var button_func func()
 
-	if check_status() == false {
-		button_text = "Harden!"
-		button_func = disable_all
-		events_text = "- Ready to harden some features of your system?\n"
-	} else {
-		button_text = "Restore..."
-		button_func = enable_all
-		events_text = "- We have already hardened some risky features, do you want to restore them?\n"
-	}
+    if check_status() == false {
+        button_text = "Harden!"
+        button_func = disable_all
+        events_text = "- Ready to harden some features of your system?\n"
+    } else {
+        button_text = "Restore..."
+        button_func = enable_all
+        events_text = "- We have already hardened some risky features, do you want to restore them?\n"
+    }
 
-	MainWindow{
-		AssignTo: &window,
-		Title:    "Harden - Security Without Borders",
-		MinSize:  Size{400, 300},
-		Layout:   VBox{},
-		Children: []Widget{
-			TextEdit{
-				AssignTo: &events,
-				Text:     events_text,
-				ReadOnly: true,
-			},
-			PushButton{
-				Text:      button_text,
-				OnClicked: button_func,
-			},
-		},
-	}.Create()
+    MainWindow{
+        AssignTo: &window,
+        Title:    "Harden - Security Without Borders",
+        MinSize:  Size{400, 300},
+        Layout:   VBox{},
+        Children: []Widget{
+            TextEdit{
+                AssignTo: &events,
+                Text:     events_text,
+                ReadOnly: true,
+            },
+            PushButton{
+                Text:      button_text,
+                OnClicked: button_func,
+            },
+        },
+    }.Create()
 
-	window.Run()
+    window.Run()
 }
--- powershell.go
+++ powershell.go
@@ -1,25 +1,25 @@
 /*
-   Hardentools
-   Copyright (C) 2017  Claudio Guarnieri, Mariano Graziano
+    Hardentools
+    Copyright (C) 2017  Claudio Guarnieri, Mariano Graziano
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package main
 
 import (
-	"golang.org/x/sys/windows/registry"
+    "golang.org/x/sys/windows/registry"
 )
 
 /*
@@ -32,27 +32,27 @@ import (
  * "3"="cmd.exe"
  */
 func trigger_powershell(enable bool) {
-	key, _, _ := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", registry.WRITE)
-	if enable {
-		events.AppendText("* Enabling Powershell and cmd\n")
-		err := key.DeleteValue("DisallowRun")
-		if err != nil {
-			events.AppendText("!! DeleteValue to enable powershell failed. Please try to start hardentools.exe as administrator!\n")
-			//panic(err)
-		}
-	} else {
-		events.AppendText("* Disabling Powershell and cmd\n")
-		key2, _, err := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\DisallowRun", registry.WRITE)
-		if err != nil {
-			events.AppendText("!! CreateKey to disable powershell failed. Please try to start hardentools.exe as administrator!\n")
-			//panic(err)
-		}
+    key, _, _ := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", registry.WRITE)
+    if enable {
+        events.AppendText("* Enabling Powershell and cmd\n")
+        err := key.DeleteValue("DisallowRun")
+        if err != nil {
+            events.AppendText("!! DeleteValue to enable powershell failed. Please try to start hardentools.exe as administrator!\n")
+            //panic(err)
+        }
+    } else {
+        events.AppendText("* Disabling Powershell and cmd\n")
+        key2, _, err := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\DisallowRun", registry.WRITE)
+        if err != nil {
+            events.AppendText("!! CreateKey to disable powershell failed. Please try to start hardentools.exe as administrator!\n")
+            //panic(err)
+        }
 
-		key.SetDWordValue("DisallowRun", 0x1)
-		key2.SetStringValue("1", "powershell_ise.exe")
-		key2.SetStringValue("2", "powershell.exe")
-		key2.SetStringValue("3", "cmd.exe")
-		key2.Close()
-	}
-	key.Close()
+        key.SetDWordValue("DisallowRun", 0x1)
+        key2.SetStringValue("1", "powershell_ise.exe")
+        key2.SetStringValue("2", "powershell.exe")
+        key2.SetStringValue("3", "cmd.exe")
+        key2.Close()
+    }
+    key.Close()
 }
