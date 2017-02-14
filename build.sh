81b0cb61c6dc209f12e12b22963f8b94b67ed418
Nex <nex@nex.sx>
2017-02-14 01:19:13 +0100

    Building with go 1.8 because of changes in walk

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@
 # go get golang.org/x/sys/windows/registry
 
 $GOPATH/bin/rsrc -manifest harden.manifest -o rsrc.syso
-GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 go build --ldflags '-s -w -extldflags "-static" -H windowsgui' -o Hardentools.exe
+GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 $GOPATH/bin/go1.8rc3 build --ldflags '-s -w -extldflags "-static" -H windowsgui' -o Hardentools.exe
