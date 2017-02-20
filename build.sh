432be6c0a68b021931e9e9f28920933a16324d78
Nex <nex@nex.sx>
2017-02-20 19:07:51 +0100

    Added icons

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 # go get github.com/akavel/rsrc
 # go get golang.org/x/sys/windows/registry
 
-$GOPATH/bin/rsrc -manifest harden.manifest -o rsrc.syso
+$GOPATH/bin/rsrc -manifest harden.manifest -ico harden.ico -o rsrc.syso
 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 $GOPATH/bin/go1.8rc3 build --ldflags '-s -w -extldflags "-static" -H windowsgui' -o hardentools.exe
