08e6d3666df823789ec531b473aeff2a5963289b
Florian Probst <github@florian-probst.de>
2018-05-21 10:53:06 +0200

    add golang version 1.10 as valid compiler

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ go get gopkg.in/Knetic/govaluate.v3
 # check go version
 GO_VERSION="$(go version)"
 GO_VERSION="$(echo $GO_VERSION | awk '{print $3}')"
-if [[ $GO_VERSION == "go1.9"* ]] || [[ $GO_VERSION == "go1.8"* ]]; then
+if [[ $GO_VERSION == "go1.10"* ]] || [[ $GO_VERSION == "go1.9"* ]] || [[ $GO_VERSION == "go1.8"* ]]; then
 	$GOPATH/bin/rsrc -manifest harden.manifest -ico harden.ico -o rsrc.syso
 	GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc CGO_ENABLED=1 go build --ldflags '-s -w -extldflags "-static" -H windowsgui' -o hardentools.exe
 else
