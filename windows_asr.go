2af63c2519373c97700c3dfc3b054964af982290
obsti8383 <github@florian-probst.de>
2020-07-30 17:35:51 +0200

    removing ASR rule 01443614-CD74-433A-B99E-2ECDC07BFC25 for now

--- windows_asr.go
+++ windows_asr.go
@@ -44,7 +44,11 @@ var ruleIDArray = []string{
 	"D3E037E1-3EB8-44C8-A917-57927947596D", // Block JavaScript or VBScript from launching downloaded executable content
 	"5BEB7EFE-FD9A-4556-801D-275E5FFC04CC", // Block execution of potentially obfuscated scripts
 	"92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B", // Block Win32 API calls from Office macro
-	"01443614-CD74-433A-B99E-2ECDC07BFC25", // Block executable files from running unless they meet a prevalence, age, or trusted list criterion
+
+	// leaving the following out for now, since it also blocks hardentools
+	// and perhaps also other "not well known to Microsoft" tools
+	// "01443614-CD74-433A-B99E-2ECDC07BFC25", // Block executable files from running unless they meet a prevalence, age, or trusted list criterion
+
 	"B2B3F03D-6A65-4F7B-A9C7-1C7EF74A9BA4", // Block untrusted and unsigned processes that run from USB
 	"C1DB55AB-C21A-4637-BB3F-A12568109D35", // Use advanced protection against ransomware
 	"D1E49AAC-8F56-4280-B9BA-993A6D77406C", // Block process creations originating from PSExec and WMI commands
